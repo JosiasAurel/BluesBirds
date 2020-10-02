d2d34072a9e78e4a33a68fb1ebe108e6f13d6ed5
JosiasAurel <ndjosiasaurel@gmail.com>
2020-10-02 21:09:48 +0100

    add a button and a text component

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,16 +2,19 @@ import Link from "next/link";
 import styles from "./NavBar.module.css";
 const NavBar = () => {
   return (
-    <header className="{styles.header}">
-      <div className="{styles.header-div}">
+    <header className={styles.header}>
+      <div>
+        <h2 className={styles.logo}>BluesBirds</h2>
+      </div>
+      <div className={styles.headerdiv}>
         <Link href="/shop">
-          <a className="{styles.link}">Marketplace</a>
+          <a className={styles.link}>Marketplace</a>
         </Link>
         <Link href="/blog">
-          <a className="{styles.link}">Blog</a>
+          <a className={styles.link}>Blog</a>
         </Link>
         <Link href="/support">
-          <a className="{styles.link}">Support</a>
+          <a className={styles.link}>Support</a>
         </Link>
       </div>
     </header>
